e690528789e91e10a47981419fc30ba347668c49
XhstormR <10527522+XhstormR@users.noreply.github.com>
2020-01-24 10:40:56 +0800

    Signed-off-by: XhstormR <10527522+XhstormR@users.noreply.github.com>

--- assets/123.bat
+++ assets/123.bat
@@ -2,5 +2,6 @@
 for /f "skip=1" %%i in ('"%~dp0%main.exe"') do (
     reg add "HKLM\SOFTWARE\Wow6432Node\NetSarang\Xftp\6" /f /v MagicCode /t REG_BINARY /d "%%i"
     reg add "HKLM\SOFTWARE\Wow6432Node\NetSarang\Xshell\6" /f /v MagicCode /t REG_BINARY /d "%%i"
+    reg add "HKLM\SOFTWARE\Wow6432Node\NetSarang\Xmanager\6" /f /v MagicCode /t REG_BINARY /d "%%i"
 )
 pause
